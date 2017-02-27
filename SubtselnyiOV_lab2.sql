3c1cc1af5b1b3f99e84ac8b4888a9ae55fbd3c06
Alexander Subtselnyi <ua.tsar.ua@gmail.com>
2017-02-27 23:26:07 +0200

    Added 2 Task with Northwind

--- SubtselnyiOV_lab2.sql
+++ SubtselnyiOV_lab2.sql
@@ -1,20 +1,63 @@
---    TAsk1
---    example for MS SQL
---SELECT COUNT_BIG(*) AS 'Number of Rows' FROM [TableName];
---    example for MySQL (count() in MySQL always returns BIGINT)
---SELECT COUNT(*) FROM 'tablename';
---    and if an approximation is enough we can use:
---SHOW TABLE STATUS LIKE 'tablename';
+--    Task1
+----    example for MS SQL
+SELECT COUNT_BIG(*) AS 'Number of Rows' FROM [TableName];
+----    example for MySQL (count() in MySQL always returns BIGINT)
+SELECT COUNT(*) FROM 'tablename';
+----    and if an approximation is enough we can use:
+SHOW TABLE STATUS LIKE 'tablename';
 
 --    Task2
---SELECT LEN('SUBTSELNYI') AS 'Surname';
+SELECT LEN('Subtselnyi') AS 'Surname';
 
 --    Task3
---SELECT REPLACE('Subtselnyi Alexander Vladimirovich',' ','_') AS 'Ï²Á';
+SELECT REPLACE('Subtselnyi Alexander Vladimirovich',' ','_') AS 'Ï²Á';
 
 --    Task4
---SELECT CONCAT(LEFT([Name], 2), LEFT([Surname], 4), '@subtselnyi.com') AS 'email' FROM [TableName];
+SELECT CONCAT(LEFT([Name], 2), LEFT([Surname], 4), '@subtselnyi.com') AS 'email' FROM [TableName];
 
 --    Task5
---SELECT DATENAME(DW, '1998-03-27') AS 'Day of Birth';
+SELECT DATENAME(DW, '1998-03-27') AS 'Day of Birth';
+
+---------------------------------------Part 2
+
+
+--   Task1
+SELECT * FROM [NORTHWND].[dbo].[Products] AS PR
+RIGHT JOIN [NORTHWND].[dbo].[Categories] AS CT ON 
+		 PR.[CategoryID] = CT.[CategoryID]
+RIGHT JOIN [NORTHWND].[dbo].[Suppliers] AS SP ON
+		 SP.[SupplierID] = PR.[SupplierID];
+
+--   Task2
+SELECT * FROM [NORTHWND].[dbo].[Orders]
+WHERE ([ShippedDate] IS NULL)  AND
+      ([OrderDate] BETWEEN '1998-04-01' AND '1998-04-30');
+
+--   Task3
+SELECT DISTINCT [FirstName],
+	   [LastName] 
+FROM [NORTHWND].[dbo].[Employees] AS EMP
+JOIN [NORTHWND].[dbo].[EmployeeTerritories] AS ET ON
+     EMP.[EmployeeID] = ET.[EmployeeID]
+JOIN [NORTHWND].[dbo].[Territories] AS TT ON
+	 TT.[TerritoryID] = ET.[TerritoryID]
+JOIN [NORTHWND].[dbo].[Region] AS RG ON
+	 RG.[RegionID] = TT.[RegionID]
+WHERE RG.[RegionDescription] = 'Northern';
+
+--  Task4
+SELECT CAST(SUM([Quantity]*[UnitPrice]*(1-[Discount])) AS decimal(10,2)) AS 'Total Price'
+FROM [NORTHWND].[dbo].[Order Details] AS OD
+WHERE OD.[OrderID] IN 
+( SELECT [OrderID] FROM [NORTHWND].[dbo].[Orders] AS ORD
+  WHERE	(DATENAME(dd, ORD.[OrderDate])%2=1));	
+
+--  Task5
+SELECT ORD.[ShipAddress]
+FROM [NORTHWND].[dbo].[Orders] AS ORD
+WHERE ORD.[OrderID] IN 
+(SELECT TOP(1) [OrderID] 
+ FROM [NORTHWND].[dbo].[Order Details] AS OD
+ GROUP BY OD.[OrderID]
+ ORDER BY SUM([Quantity]*[UnitPrice]*(1-[Discount])) DESC );
 
