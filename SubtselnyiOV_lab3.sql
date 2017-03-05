73b6cc231212eac2322d2c4825b21410c0070e1e
Alexander Subtselnyi <ua.tsar.ua@gmail.com>
2017-03-06 00:14:13 +0200

    All except Task2.5

--- SubtselnyiOV_lab3.sql
+++ SubtselnyiOV_lab3.sql
@@ -17,14 +17,14 @@
 --SET @id = 1
 --WHILE (SELECT COUNT([id]) FROM [NORTHWND].[dbo].[IP-51]) >= @id
 --BEGIN
---IF ((SELECT [name] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id) != 
---	ALL(SELECT DISTINCT [name] FROM [KPI].[dbo].[IP-52]))
---	BEGIN
---		SELECT @name = (SELECT [surname] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id )
---		SET @name += (SELECT [name] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id)
---		PRINT @name
---	END
---SET @id += 1;
+--	IF ((SELECT [name] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id) != 
+--		ALL(SELECT DISTINCT [name] FROM [KPI].[dbo].[IP-52]))
+--		BEGIN
+--			SELECT @name = (SELECT [surname] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id )
+--			SET @name += (SELECT [name] FROM [KPI].[dbo].[IP-51] WHERE [id] = @id)
+--			PRINT @name
+--		END
+--	SET @id += 1
 --END
 
 
@@ -42,7 +42,7 @@
 --			WHEN 8 THEN 'EIGHT'
 --			WHEN 9 THEN 'NINE'
 --			ELSE CAST([NUMBER] AS varchar)
---			END
+--END
 --FROM [NORTHWND].[dbo].[NUMBERS]
 
 
@@ -56,3 +56,47 @@
 
 -----------------------------PART2
 --   Task1
+----Create table without SHipVia column
+--SELECT * INTO #TemporaryTable FROM [NORTHWND].[dbo].[Orders]
+--ALTER TABLE #TemporaryTable DROP COLUMN [ShipVia]
+----Joining 2 tables
+--SELECT #TemporaryTable.*, (CASE NT.[ShipVia]
+--								WHEN 1 THEN 'Subtselnyi'
+--								WHEN 2 THEN 'Oleksandr'
+--								WHEN 3 THEN 'Volodymyrovich'
+--								ELSE CAST([ShipVia] AS varchar)
+--								END) AS 'ShipVia' 
+--						   FROM  #TemporaryTable
+--JOIN [NORTHWND].[dbo].[Orders] AS NT
+--ON #TemporaryTable.[OrderID] = NT.[OrderID]
+--DROP TABLE #TemporaryTable
+
+--   Task2
+--SELECT [Country] FROM [NORTHWND].[dbo].[Customers]
+--UNION
+--SELECT [Country] FROM [NORTHWND].[dbo].[Employees]
+--UNION
+--SELECT [ShipCountry] FROM [NORTHWND].[dbo].[Orders]
+--ORDER BY [Country]
+
+--   Task3
+--SELECT [FirstName], [LastName], COUNT(*)  FROM [NORTHWND].[dbo].[Employees] AS EMP
+--LEFT JOIN [NORTHWND].[dbo].[Orders] AS ORD
+--ON EMP.[EmployeeID] = ORD.[EmployeeID]
+--WHERE YEAR(ORD.[OrderDate]) = 1998 AND MONTH(ORD.[OrderDate]) <=3
+--GROUP BY EMP.[LastName], EMP.[FirstName]
+
+
+--   Task4
+--WITH OrderCTE ([OrderID]) AS (
+--	SELECT DISTINCT [OrderID]
+--	FROM [NORTHWND].[dbo].[Order Details] 
+--	WHERE [ProductID] = ANY(SELECT DISTINCT [ProductID]
+--							FROM [NORTHWND].[dbo].[Order Details]
+--							GROUP BY [ProductID]
+--							HAVING SUM([Quantity]) > 100)
+--	AND ([Discount] < (SELECT MAX([Discount]) 
+--				      FROM [NORTHWND].[dbo].[Order Details])))
+
+--SELECT * FROM [NORTHWND].[dbo].[Orders] 
+--WHERE [OrderID] IN (SELECT [OrderID] FROM OrderCTE)
